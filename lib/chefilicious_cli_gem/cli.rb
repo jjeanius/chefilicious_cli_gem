e464ffc547e6ca843648c9ada4fd4020e091b83e
Judy McGoldrick <jjeanius@verizon.net>
2018-03-17 21:00:41 +0000

    case statments for cli codes

--- lib/chefilicious_cli_gem/cli.rb
+++ lib/chefilicious_cli_gem/cli.rb
@@ -13,70 +13,163 @@ class ChefiliciousCliGem::CLI
 
   def meal_type
     puts "What are you looking for?"
-    puts "Please enter a number or type exit to leave the program
-
+    puts "Please enter a number or type exit for exiting
           1. Breakfast
           2. Lunch
           3. Dinner
           4. Desert"
-    whiile input != "exit"
       input = gets.strip
+
       case input
-      when "1"
-        puts "Breakfast"
-      when "2"
-        puts "Lunch"
-      when "3"
-        puts "Dinner"
-      when "4"
-        puts "Yummy Desert!"
+        when "1"
+          puts "Breakfast"
+        when "2"
+          puts "Lunch"
+        when "3"
+          puts "Dinner"
+        when "4"
+          puts "Yummy Desert!"
+        when "exit"
+          exit
       end
     end
 
   def cooking_time
-    puts "Please select your desire cooking time:
+    puts "Please select your desire cooking time:"
+    puts "type exit for exiting and p for Previous Menu
           1. Less than 20 minutes
           2. Between 21 to 45 minutes
           3. Between 45 to 60 minutes
           4. More than 60 Minutes"
     input = gets.strip
+
+    case input
+      when "1"
+        puts "Less than 20 minutes"
+      when "2"
+        puts "Between 21 to 45 minutes"
+      when "3"
+        puts "Between 45 to 60 minutes"
+      when "4"
+        puts "More than 60 Minutes"
+      when "exit"
+        exit
+      when "p"
+        meal_type
+        cooking_time
+    end
   end
 
   def allergen
-    puts "Are you allergic to any of the following ingredientes?
+    puts "Are you allergic to any of the following ingredientes?"
+    puts "type exit for exiting and p for Previous Menu
           1. Shellfish
           2. Dairy
           3. Egg
-          4. Fish
-          5. Peanut
-          6. Soy
-          7. Tree Nut
-          8. Wheat
-          9. No Allergens"
+          4. Nuts
+          5. Soy
+          6. Wheat
+          7. No Allergens"
     input = gets.strip
+
+    case input
+      when "1"
+        puts "Shellfish"
+      when "2"
+        puts "Dairy"
+      when "3"
+        puts "Egg"
+      when "4"
+        puts "Nuts"
+      when "5"
+        puts "Soy"
+      when "6"
+        puts "Wheat"
+      when "exit"
+        exit
+      when "p"
+        meal_type
+        cooking_time
+        allergen
+    end
   end
 
   def cuisine
-    puts "Please select a cuisine:
+    puts "Please select a cuisine:"
+    puts "type exit for exiting and p for Previous Menu
           1. American
           2. Asian (Chinese, Korean, Japanese, Thai, Vietnamese, Indian)
           3. Latin (Mexican)
-          4. Mediterranean (Greek, Spanish, French, Italian, Middle Eastern)
-          E. or e. Exit
-          P. or p. Previous Menu"
+          4. Mediterranean (Greek, Spanish, French, Italian, Middle Eastern)"
     input = gets.strip
+
+    case input
+      when "1"
+        puts "American"
+      when "2"
+        puts "Asian"
+      when "3"
+        puts "Latin"
+      when "4"
+        puts "Mediterranean"
+      when "exit"
+        exit
+      when "p"
+        meal_type
+        cooking_time
+        allergen
+        Cuisine
+    end
   end
 
   def food_category
-    puts "What do you like to cook?
+    puts "What do you like to cook?"
+    puts "type exit for exiting and p for Previous Menu
           1. Meat (Beef, Chicken, Lamb, Pork)
-          2. Fish
+          2. Seafood
           3. Poultry
           4. Vegetable
           5. Grains/Pasta
-          6. Sweets
-          7. Special Dietary Needs"
+          6. Sweets"
     input = gets.strip
+
+    case input
+      when "1"
+        puts "Meat"
+      when "2"
+        puts "Seafood"
+      when "3"
+        puts "Poultry"
+      when "4"
+        puts "Vegetable"
+      when "5"
+        puts "Grains/Pasta"
+      when "6"
+        puts "Sweets"
+      when "exit"
+        exit
+      when "p"
+        meal_type
+        cooking_time
+        allergen
+    end
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   end
 
 end

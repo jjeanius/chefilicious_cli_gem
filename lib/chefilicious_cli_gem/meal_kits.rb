594e6fe184ff9364e5112022d68356b5f461f92c
Judy McGoldrick <jjeanius@verizon.net>
2018-03-19 04:01:04 +0000

    adding url

--- lib/chefilicious_cli_gem/meal_kits.rb
+++ lib/chefilicious_cli_gem/meal_kits.rb
@@ -1,5 +1,5 @@
 class ChefiliciousCliGem::Meal_Kits
-  attr_accessor :name, :price, :skill_level, :cooking_time, :allergen
+  attr_accessor :name, :price, :skill_level, :cooking_time, :allergen, :url
 
   def self.selection
     puts "listing meal_kits"
@@ -12,6 +12,7 @@ class ChefiliciousCliGem::Meal_Kits
     meal_kit_1.skill_level = "easy, intermedian, hard"
     meal_kit_1.cooking_time = "minutes"
     meal_kit_1.allergen = "allergen"
+    meal_kit_1.url = "https://www.chefd.com/collections/top-rated"
 
     meal_kit_2 = self.new
     meal_kit_2.name = "Fresh_Direct"
@@ -19,6 +20,7 @@ class ChefiliciousCliGem::Meal_Kits
     meal_kit_2.skill_level = "easy, intermedian, hard"
     meal_kit_2.cooking_time = "minutes"
     meal_kit_2.allergen = "allergen"
+    meal_kit_2.url = "https://www.freshdirect.com/browse.jsp?pageType=browse&id=meals_kits_meals"
 
     meal_kit_3 = self.new
     meal_kit_3.name = "Chefday!"
@@ -26,6 +28,7 @@ class ChefiliciousCliGem::Meal_Kits
     meal_kit_3.skill_level = "easy, intermedian, hard"
     meal_kit_3.cooking_time = "minutes"
     meal_kit_3.allergen = "allergen"
+    meal_kit_3.url = "http://www.chefday.com/recipes"
   end
 
 end

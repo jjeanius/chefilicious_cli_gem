bd8ec4df65b0a7455db5ea519acbcda1a558c7ee
Judy McGoldrick <jjeanius@verizon.net>
2018-04-12 02:46:54 +0000

    correcting errors

--- lib/chefilicious_cli_gem/chefs.rb
+++ lib/chefilicious_cli_gem/chefs.rb
@@ -1,5 +1,5 @@
 class Chefs
-  attr_accessor :name, :knowfor, :description, :chef, :chef_info
+  attr_accessor :name, :knowfor, :cuisine, :chef, :chef_info
 
   @@all_chefs = []
 
@@ -13,15 +13,15 @@ class Chefs
 
   def self.scrape_chefs
     doc = Nokogiri::HTML(open("https://www.chefd.com/pages/our-chefs"))
-    meal_kits_chefs = doc.css("div.section-width-limiter #chefsGrid")  #doc.css("div.section-width-limiter div#chefsGrid.chef-grid")
+    meal_kits_chefs = doc.css("div.section-width-limiter .chef-grid-item-wrapper")  #doc.css("div.section-width-limiter div#chefsGrid.chef-grid")
     #binding.pry
+    sleep 10
     meal_kits_chefs.each do|chef|
-   binding.pry
+  binding.pry
       chef_info = self.new
-    #  binding.pry
-      chef_info.name = chef.css(".chef-name").inner_html
-      chef_info.knowfor = chef.css(".chef-description")
-      chef_info.description = chef.css("p.description")
+      chef_info.name = chef.css(" .chef-details .chef-name").children.inner_html
+      chef_info.cuisine = chef.css(".chef-description")
+      chef_info.knowfor = chef.css(".chef-subtitle")
       chef_info.url = "https://www.chefd.com/collections/#{chef.css('a').attribute('href').text.strip}"
       chef_info
       @@all_chefs << chef
